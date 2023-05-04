9e9375f8203fefd40b8c48c551c71ae487d162a6
Giovanni Maiorana <giovanni.maiorana.14@gmail.com>
2023-05-04 16:43:44 +0200

    Esercizio + bonus completo

--- Models/listFilm.php
+++ Models/listFilm.php
@@ -3,6 +3,7 @@
 $avatar = new Movie ('Avatar','4/5','Fantasy');
 $pinocchio = new Movie ('Pinocchio','3/5','Fantasy');
 $lovebugs = new Movie ('Love Bugs','3/5','Romantic');
+$biancaneve = new Movie ('Biancaneve','5/5','Cartoon');
 
-$listMovie=[$avatar,$pinocchio,$lovebugs]
+$listMovie=[$avatar,$pinocchio,$lovebugs,$biancaneve]
 ?>
--- index.php
+++ index.php
@@ -2,11 +2,42 @@
 require __DIR__ . '/Models/Movie.php';
 require __DIR__ . '/Models/listFilm.php';
 
-
-
-
-echo $avatar->getInfo();
-echo $pinocchio->getInfo();
-echo $lovebugs->getInfo();
-
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Elenco Film</title>
+    <link rel="stylesheet" href="/style/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+<body>
+    
+<table class="table table-striped">
+<thead>
+    <tr>
+    <th scope="row"></th>
+      <th scope="col">TITLE</th>
+      <th scope="col">VOTE</th>
+      <th scope="col">genres</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+    foreach($listMovie as $movie){ ?>
+        <tr>
+        <th scope="row"></th>
+            <td> <?php echo $movie->title; ?></td>
+            <td> <?php echo $movie->vote; ?></td>
+            <td > <?php echo $movie->genres; ?></td>
+           
+        </tr>
+ <?php   } ?>
+
+</table>
+</body>
+</html>
